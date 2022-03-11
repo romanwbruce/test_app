0a24750b91fa70af9c58f8fdde8e5687d0107823
Roman <71509815+romanwbruce@users.noreply.github.com>
2022-03-10 22:44:56 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('This project was deployed with HEDGS.');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
