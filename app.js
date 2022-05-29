8d2f55ae523046f7ccfe4a80fb7215a04071f927
Roman <71509815+romanwbruce@users.noreply.github.com>
2022-05-29 13:15:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('This project was deployed with HEDGS.');
+  res.send('<h1>This is a website, that is deployed on <a href="freeapphosting.net">freeapphosting.net</a> (Totally free!)</h1>');
 });
 
 app.listen(PORT, HOST);
